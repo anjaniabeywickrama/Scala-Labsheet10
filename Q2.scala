45fed56c3306d2b01fe96320efe2ad030c5d336b
Anjani Abeywickrama <135728584+anjaniabeywickrama@users.noreply.github.com>
2023-09-01 11:44:33 +0530

    Add files via upload

---
+++ Q2.scala
@@ -0,0 +1,16 @@
+package Labsheet10
+
+object Q2 {
+  def countLetterOccurrences(input: List[String]): Int = {
+    val Lengths = input.map(_.length)
+    val totalOccurrences = Lengths.reduce((total, temp) => total + temp)
+    totalOccurrences
+  }
+
+  def main(args: Array[String]): Unit = {
+    val input = List("apple", "banana", "cherry", "date")
+    val totalCount = countLetterOccurrences(input)
+    println(s"Total count of letter occurrences: $totalCount")
+  }
+
+}
